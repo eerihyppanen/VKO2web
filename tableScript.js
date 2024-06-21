d75bdcfbfbd969f608dbb4036785461d9536e253
eerihyppanen <eeri.hyppanen@student.lut.fi>
2024-06-21 12:22:46 +0300

    muutoksiaaaaa

--- tableScript.js
+++ tableScript.js
@@ -9,7 +9,8 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
     const imageInput = document.getElementById("input-image");
     const file = imageInput.files[0];
 
-    
+    const imageSrc = URL.createObjectURL(file);
+
     const tableBody = document.querySelector("#user-table tbody");
     const rows = Array.from(tableBody.getElementsByTagName("tr"));
     
@@ -19,10 +20,12 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
             
             const emailCell = row.getElementsByTagName("td")[1];
             const adminCell = row.getElementsByTagName("td")[2];
+            const imageCell = row.getElementsByTagName("td")[3];
             
             usernameCell.textContent = username;
             emailCell.textContent = email;
             adminCell.textContent = isAdmin;
+            imageCell.innerHTML = `<img src="${imageSrc}" alt="User Image" width="64" height="64">`;
             return true; 
         }
         return false;
@@ -42,12 +45,14 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
         usernameCell.textContent = username;
         emailCell.textContent = email;
         adminCell.textContent = isAdmin;
+        imageCell.innerHTML = `<img src="${imageSrc}" alt="User Image" width="64" height="64">`;
         
 
         
         newRow.appendChild(usernameCell);
         newRow.appendChild(emailCell);
         newRow.appendChild(adminCell);
+        newRow.appendChild(imageCell);
         
 
         
@@ -61,9 +66,8 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
 function emptyTable() {
     
     const tableBody = document.querySelector("#user-table tbody");
+    tableBody.innerHTML = "";
     
     
-    while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild);
-    }
+    
 }
